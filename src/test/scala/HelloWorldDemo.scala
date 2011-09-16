70fdeb305858e594124d57d346fd318b4a09240e
Christian Buschmann <christian@snowblack.de>
2011-09-17 00:29:37 +0200

    documentation on HelloWorld

--- src/test/scala/HelloWorldDemo.scala
+++ src/test/scala/HelloWorldDemo.scala
@@ -15,15 +15,25 @@ object HelloWorldDemo {
     * If ''--startVal'' option is specified than add everything else to this value */
     def calcParser( params: List[String] ): CmdLineParser[Int] = 
       for{
-        startVal <- startValOption //Read the result of ''--startVal'' as Int
+        startVal <- startValOption //Read the parameter of ''--startVal'' as Int
       }yield params.foldLeft( startVal )( ( x, y ) => x + y.toInt ) // Here the calculation takes place. Everything is summed up
     
-    // Here all Commandline options and their corresponding functions will be declared
+    //The following three line defines all commandline options for this program and 
+    //their corresponding functions 
+    //Ín general you define an option together with a function which will be evaluated,
+    //if the option is given on command line. A default function provides a value, if
+    //the option is not given.
     /** Define the help option */
     val helpOption = so( List( "-h", "--help" ), "Print this help", help( "HelloWorldDemo", "You can add and subtract..." ) )
-    /** Define the start value. If this option is set, than return the number specified on command line, otherwise return 0 */
+
+    /** Define the start value. If this option is set, than return the number specified 
+    * on command line (_.toInt), otherwise return ''0'' as default
+    * ''sop'' is short for (s)ingle (o)ption with (p)arameter
+    * There is also ''so'' (s)ingle (o)ption and ''sopl'' (s)ingle (o)ption with (p)arameter (l)ist*/
     val startValOption = sop( "--startVal", "<int>", "Start value for addition", _.toInt, 0 )
-    /** Defines the function which takes all parameters. In this case all parameters will be added */
+
+    /** Defines the function which takes all parameters. In this case all parameters will 
+    * be treated as numbers and added */
     val programParser = parameters( "<int> <int> ...", "Values to add", calcParser ) 
     }
 
